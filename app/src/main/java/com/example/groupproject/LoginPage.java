048e52b7e21d1cfb4eacdc663a5ffeaecf2b18e8
not-a-thingy <76759140+not-a-thingy@users.noreply.github.com>
2021-01-21 16:22:54 +0800

    Library update
    changed library to compatible with androidx

--- app/src/main/java/com/example/groupproject/LoginPage.java
+++ app/src/main/java/com/example/groupproject/LoginPage.java
@@ -1,6 +1,6 @@
 package com.example.groupproject;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginPage extends AppCompatActivity {
