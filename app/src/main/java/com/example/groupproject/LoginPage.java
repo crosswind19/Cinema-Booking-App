ce53aaf87cde8d50eb796d45cc42afa60963ea49
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2021-01-21 12:55:39 +0800

    create login page

---
+++ app/src/main/java/com/example/groupproject/LoginPage.java
@@ -0,0 +1,13 @@
+package com.example.groupproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
