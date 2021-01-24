5acf88b2593f4aaadab2db552793ff69e11a6c7b
nothing <nothing@horsefucker.org>
2021-01-24 19:47:10 +0800

    forgot password function done

--- app/src/main/java/com/example/groupproject/activity_forgotpassword.java
+++ app/src/main/java/com/example/groupproject/activity_forgotpassword.java
@@ -1,14 +1,61 @@
 package com.example.groupproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class activity_forgotpassword extends AppCompatActivity {
+
+    private EditText passwordEmail;
+    private Button resetPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpassword);
+
+        passwordEmail = (EditText)findViewById(R.id.tv_sendemailtoresetpass);
+        resetPassword = (Button)findViewById(R.id.btn_reset);
+        //database function
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //fetching user email, convert to string then remove all whitespaces.
+                String useremail = passwordEmail.getText().toString().trim();
+
+                if(useremail.equals("")) {
+                    //if user does not enter anything into the text box
+                    Toast.makeText(activity_forgotpassword.this, "Please enter your registered email ID.", Toast.LENGTH_LONG).show();
+                } else {
+                    firebaseAuth.sendPasswordResetEmail(useremail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                //user enters a registered email
+                                Toast.makeText(activity_forgotpassword.this, "Password reset email sent.", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(new Intent(activity_forgotpassword.this, activity_login.class));
+                            } else {
+                                //if user enters a not registered email.
+                                Toast.makeText(activity_forgotpassword.this, "Error in sending reset password email.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
