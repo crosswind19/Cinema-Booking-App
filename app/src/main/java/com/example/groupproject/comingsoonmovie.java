6d6503d089a1572463c651e03796751e42f8176d
zhfoong <77926846+zhfoong@users.noreply.github.com>
2021-02-17 01:44:15 +0800

    need edit ask me do ya

---
+++ app/src/main/java/com/example/groupproject/comingsoonmovie.java
@@ -0,0 +1,14 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class comingsoonmovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comingsoonmovie);
+    }
+}
