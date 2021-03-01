8585b54f73438c196dc4e1aa462e3b91c7eb09e6
zhfoong <77926846+zhfoong@users.noreply.github.com>
2021-03-01 15:42:20 +0800

    try

---
+++ app/src/main/java/com/example/groupproject/ViewHolders/Feedbacks.java
@@ -0,0 +1,22 @@
+package com.example.groupproject.ViewHolders;
+
+public class Feedbacks {
+    private String Username;
+    private String Feedback;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getFeedback(String feedback) {
+        return Feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        Feedback = feedback;
+    }
+}
---
+++ app/src/main/java/com/example/groupproject/feedback.java
@@ -0,0 +1,41 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.groupproject.ViewHolders.Feedbacks;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class feedback extends AppCompatActivity {
+
+    EditText txtusername, txtfeedback;
+    Button btnsubmit;
+    DatabaseReference reff;
+    Feedbacks feedbacks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        txtusername=(EditText)findViewById(R.id.txtusername);
+        txtfeedback=(EditText)findViewById(R.id.txtfeedback);
+        btnsubmit=(Button)findViewById(R.id.btnsubmit);
+        feedbacks=new Feedbacks();
+        reff= FirebaseDatabase.getInstance().getReference().child("Feedbacks");
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                feedbacks.setUsername(txtusername.getText().toString().trim());
+                feedbacks.getFeedback(txtfeedback.getText().toString().trim());
+                reff.push().setValue(feedbacks);
+                //Toast.makeText(datainsert.this, "data inserted sucessfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
