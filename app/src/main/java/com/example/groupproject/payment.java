29e9611914af3f5765b6a670edb6000c05d9d108
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2021-03-10 19:05:06 +0800

    payment

--- app/src/main/java/com/example/groupproject/payment.java
+++ app/src/main/java/com/example/groupproject/payment.java
@@ -2,10 +2,10 @@ package com.example.groupproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,30 +22,39 @@ public class payment extends AppCompatActivity {
         cash = (Button)findViewById(R.id.cash);
         card = (Button)findViewById(R.id.card);
 
-        card.setOnClickListener( new View.OnClickListener() {
+        card.setEnabled(false);
+
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                EditText userName = (EditText) findViewById(R.id.NAME);
-                EditText phoneNumber = (EditText) findViewById(R.id.phoneNumber);
-                EditText email = (EditText) findViewById(R.id.email);
-                EditText cardnumber = (EditText) findViewById(R.id.cardNumber);
-                EditText date = (EditText) findViewById(R.id.date);
-                EditText ccv = (EditText) findViewById(R.id.ccv);
-
-
-                if( TextUtils.isEmpty(userName.getText()) || TextUtils.isEmpty(phoneNumber.getText()) || TextUtils.isEmpty(email.getText()) ||
-                        TextUtils.isEmpty(cardnumber.getText()) || TextUtils.isEmpty(date.getText()) || TextUtils.isEmpty(ccv.getText())){
-                    /**
-                     *   You can Toast a message here that the Username is Empty
-                     **/
-                    userName.setError( "First name is required!" );
-
-                }else{
-                    Intent i = new Intent(payment.this, payCard.class);
-                    startActivity(i);
-                }
+                Toast.makeText(payment.this,"Card payment is not available",Toast.LENGTH_SHORT).show();
             }
-
         });
+
+//        card.setOnClickListener( new View.OnClickListener() {
+//            public void onClick(View v) {
+//                EditText userName = (EditText) findViewById(R.id.NAME);
+//                EditText phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+//                EditText email = (EditText) findViewById(R.id.email);
+//                EditText cardnumber = (EditText) findViewById(R.id.cardNumber);
+//                EditText date = (EditText) findViewById(R.id.date);
+//                EditText ccv = (EditText) findViewById(R.id.ccv);
+//
+//
+//                if( TextUtils.isEmpty(userName.getText()) || TextUtils.isEmpty(phoneNumber.getText()) || TextUtils.isEmpty(email.getText()) ||
+//                        TextUtils.isEmpty(cardnumber.getText()) || TextUtils.isEmpty(date.getText()) || TextUtils.isEmpty(ccv.getText())){
+//                    /**
+//                     *   You can Toast a message here that the Username is Empty
+//                     **/
+//                    userName.setError( "First name is required!" );
+//
+//                }else{
+//                    Intent i = new Intent(payment.this, payCard.class);
+//                    startActivity(i);
+//                }
+//            }
+//
+//        });
 
         cash.setOnClickListener(new View.OnClickListener() {
             @Override
