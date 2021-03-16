1bc7b8f0ab35daa73a3b363c3d26996a1ebfe709
not-a-thingy <76759140+not-a-thingy@users.noreply.github.com>
2021-03-16 19:40:25 +0800

    Placeholder for splash screen

---
+++ app/src/main/java/com/example/groupproject/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler;
+    //FirebaseAuth fAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    startActivity(new Intent(getApplicationContext(), activity_login.class));
+                    finish();
+            }
+        },4000);
+    }
+}
