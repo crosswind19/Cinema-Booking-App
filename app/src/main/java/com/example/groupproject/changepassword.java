80eeec7fddfdc4262e1ae005e823726b0047639a
nothing <nothing@horsefucker.org>
2021-03-16 18:24:42 +0800

    reedit

--- app/src/main/java/com/example/groupproject/changepassword.java
+++ app/src/main/java/com/example/groupproject/changepassword.java
@@ -19,10 +19,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class changepassword extends AppCompatActivity {
 
     private Button reset;
-    private EditText oldPassword, newPassword, reNewPassword;
+    private EditText newPassword;
     private FirebaseUser firebaseUser;
-    //private String oldpass, newpass, renew;
-    //private Boolean result = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,40 +28,11 @@ public class changepassword extends AppCompatActivity {
         setContentView(R.layout.activity_changepassword);
 
         reset = findViewById(R.id.btn_reset);
-        oldPassword = findViewById(R.id.et_oldpass);
         newPassword = findViewById(R.id.et_newpass);
-        reNewPassword = findViewById(R.id.et_renew);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        /*oldpass = oldPassword.getText().toString();
-        newpass = newPassword.getText().toString();
-        renew = reNewPassword.getText().toString();
-
-
-        if (TextUtils.isEmpty(oldpass)) {
-            oldPassword.setError("Old Password can't be empty.");
-        }
-
-        else if (TextUtils.isEmpty(newpass)) {
-            newPassword.setError("New Password can't be empty.");
-        }
-
-        else if (newpass.length() < 8) {
-            newPassword.setError("New Password must be more than 7 Characters");
-        }
-
-        else if (TextUtils.isEmpty(renew)){
-            reNewPassword.setError("This field can't be empty.");
-        }
-
-        else if (!TextUtils.equals(newpass, renew)){
-            newPassword.setError("Password does not match.");
-            reNewPassword.setError("Password does not match.");
-        }else
-            result = true;*/
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
